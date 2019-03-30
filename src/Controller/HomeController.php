aaf45660d0104af7bd112becda9d5ce399f9868b
Saphyel <Saphyel@gmail.com>
2019-03-30 20:10:07 +0000

    Add dev vendors

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,9 +5,13 @@ declare(strict_types=1);
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
 
 final class HomeController extends AbstractController
 {
+    /**
+     * @Route("/")
+     */
     public function index() {
         return $this->json('hola');
     }
