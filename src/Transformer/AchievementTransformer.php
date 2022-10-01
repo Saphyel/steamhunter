9186d72e7bee627aa2e71ce55f82d8b321233e06
Saphyel <Saphyel@gmail.com>
2022-10-01 09:37:07 +0100

    Upgrade Symfony to 6.1

--- src/Transformer/AchievementTransformer.php
+++ src/Transformer/AchievementTransformer.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Transformer;
 
 use App\Model\Achievement;
+
 use function array_key_exists;
 
 final class AchievementTransformer implements TransformerInterface
